90ad5909e4931f2fb8b5212c9eb3994f77e7ac90
JakobSchauser <jakob.schauser@gmail.com>
2021-10-26 18:07:40 +0200

    maybe now

--- photos.js
+++ photos.js
@@ -1,5 +1,5 @@
 for (i = 0; i < 26670;i+=10){
-    nms[i] = "frames/frame"+String(i)
+    nms[i] = "frames/frame"+String(i)+"jpg"
 }
 
 var arr = new collection(nms);
@@ -7,12 +7,18 @@ var arr = new collection(nms);
 function collection(imgs) {
   this.imgs = imgs;
   this.i = 0;
+
   this.next = function(element) {
-    var img = document.getElementById('element')
+    var img = document.getElementById(element);
+    
     this.i++;
-    if (this.i == imgs.length) {
+    if (this.i >= imgs.length) {
       this.i = 0;
     }
-    img.src = imgs[this.i].src;
-  }
+
+    img.src = imgs[this.i];
+  };
+
+  
+  this.next("frames/frame0.jpg"); // to initialize with some image
 }
