30d7f9ce2cdeb8ea160a46818aae83e0d408cbcb
JakobSchauser <jakob.schauser@gmail.com>
2021-10-27 12:00:37 +0200

    added new images

--- photos.js
+++ photos.js
@@ -1,9 +1,10 @@
 window.onload = choose_random;
 
 var mypics = new Array()
-for (i = 0; i < 1898;i+=1){
-    mypics[i] = "frames/frame"+String(i*10)+".jpg"
-}
+// for (i = 0; i < 1898;i+=1){
+//     mypics[i] = "frames/frame"+String(i*10)+".jpg"
+// }
+
 
 function choose_random() {
   var randomNum = Math.floor(Math.random() * mypics.length);
