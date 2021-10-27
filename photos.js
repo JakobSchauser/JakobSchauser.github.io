433b132179c14d4fdacdb6083dcae8dabec29c85
JakobSchauser <jakob.schauser@gmail.com>
2021-10-27 12:25:35 +0200

    work again pls?

--- photos.js
+++ photos.js
@@ -7,7 +7,7 @@ function choose_random() {
 
   var mypics = new Array();
   for (i = 0; i < 1000;i+=1){
-      mypics[i] = "github.com/JakobSchauser/JakobSchauser.github.io/blob/master/frames/frame"+String(i)+".jpg?raw=true";
+      mypics[i] = "http//www.github.com/JakobSchauser/JakobSchauser.github.io/blob/master/frames/frame"+String(i)+".jpg?raw=true";
   }
 
   var randomNum = Math.floor(Math.random() * mypics.length);
