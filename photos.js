9afefc63d04c5764ae0c36a8144958f5ef8f2c1e
JakobSchauser <jakob.schauser@gmail.com>
2021-10-27 12:05:42 +0200

    printing

--- photos.js
+++ photos.js
@@ -7,6 +7,8 @@ for (i = 0; i < 1000;i+=1){
 
 
 function choose_random() {
+  
   var randomNum = Math.floor(Math.random() * mypics.length);
+  console.log(mypics[randomNum]);
   document.getElementById("mainImg").src = mypics[randomNum];
 }
