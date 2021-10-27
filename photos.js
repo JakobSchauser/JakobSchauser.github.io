eb6c268f831c8943a08084cf251e4304043eb436
JakobSchauser <jakob.schauser@gmail.com>
2021-10-27 12:01:23 +0200

    changed counting

--- photos.js
+++ photos.js
@@ -1,9 +1,9 @@
 window.onload = choose_random;
 
 var mypics = new Array()
-// for (i = 0; i < 1898;i+=1){
-//     mypics[i] = "frames/frame"+String(i*10)+".jpg"
-// }
+for (i = 0; i < 1000;i+=1){
+    mypics[i] = "frames/frame"+String(i)+".jpg"
+}
 
 
 function choose_random() {
