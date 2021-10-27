918b8c0dea15f3a3e027b48fff933735aa6da3fd
JakobSchauser <jakob.schauser@gmail.com>
2021-10-27 12:08:23 +0200

    yeabo

--- photos.js
+++ photos.js
@@ -1,13 +1,15 @@
 window.onload = choose_random;
 
-var mypics = new Array()
-for (i = 0; i < 1000;i+=1){
-    mypics[i] = "frames/frame"+String(i)+".jpg"
-}
 
 
 function choose_random() {
-  
+  console.log("mypics[randomNum]");
+
+  var mypics = new Array()
+  for (i = 0; i < 1000;i+=1){
+      mypics[i] = "frames/frame"+String(i)+".jpg"
+  }
+
   var randomNum = Math.floor(Math.random() * mypics.length);
   console.log(mypics[randomNum]);
   document.getElementById("mainImg").src = mypics[randomNum];
