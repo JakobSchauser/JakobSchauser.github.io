20f7ae45edd3025fe0cd8659dcbb3ecc5d907584
JakobSchauser <jakob.schauser@gmail.com>
2021-10-27 12:08:47 +0200

    yeahbo

--- photos.js
+++ photos.js
@@ -5,9 +5,9 @@ window.onload = choose_random;
 function choose_random() {
   console.log("mypics[randomNum]");
 
-  var mypics = new Array()
+  var mypics = new Array();
   for (i = 0; i < 1000;i+=1){
-      mypics[i] = "frames/frame"+String(i)+".jpg"
+      mypics[i] = "frames/frame"+String(i)+".jpg";
   }
 
   var randomNum = Math.floor(Math.random() * mypics.length);
